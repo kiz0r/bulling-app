0d8da8fa00faa9a39f220ee3f7024c4677f8ad5e
Kirill Khomenko <kkhomenko10@gmail.com>
2023-03-08 00:12:49 +0100

    advanced

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const name = prompt('Enter Name of person to bull');
+let name = prompt('Enter Name of person to bull');
 
 const bullingBtn = document.querySelector('button');
 const bullingArea = document.querySelector('p');
 
+function validateName(name) {}
+
 bullingBtn.onclick = () => {
-  bullingArea.innerHTML += `${name}'s stupid bitch. </br>`.toUpperCase();
+  if (name.match(/.*(ler|лер).*/gi)) {
+    bullingArea.innerHTML += `I love ${name} </br>`.toUpperCase();
+  } else if (name.match(/.*(kir|кир|ко|ko|kon|кон)/gi)) {
+    alert('на ебале себе это напиши. даун блять.');
+    name = 'Roma and Andrii';
+    bullingArea.innerHTML += `${name}'re stupid bitches. </br>`.toUpperCase();
+  } else if (name === 'Roma and Andrii') {
+    bullingArea.innerHTML += `${name}'re stupid bitches. </br>`.toUpperCase();
+  } else {
+    bullingArea.innerHTML += `${name}'s stupid bitch. </br>`.toUpperCase();
+  }
 };
